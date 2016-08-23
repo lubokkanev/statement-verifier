f3574be0d81693ff832e275ea9d8f61e7c948f16
lubokkanev <LuboKKanev@gmail.com>
2016-08-24 00:17:28 +0300

    Add method to retrieve all variables
    Needed in the UI, when diplaying the current variables.

--- src/com/nvl/variable/manager/VariableManager.java
+++ src/com/nvl/variable/manager/VariableManager.java
@@ -1,9 +1,15 @@
 package com.nvl.variable.manager;
 
+import com.nvl.variable.Variable;
+
+import java.util.Set;
+
 public interface VariableManager {
     void addVariable(String name, String value);
 
     void removeVariable(String name);
 
     void updateVariable(String name, String value);
+
+    Set<Variable> variables();
 }
