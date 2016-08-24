49b08f593231cc886297e1a592ba5b64b752dccd
lubokkanev <LuboKKanev@gmail.com>
2016-08-24 23:37:24 +0300

    Create verificator class

---
+++ src/com/nvl/NikiClass.java
@@ -0,0 +1,11 @@
+package com.nvl;
+
+import com.nvl.variable.Variable;
+
+import java.util.Set;
+
+public interface NikiClass {
+    String niki(String userInput);
+
+    Set<Variable> variables();
+}
