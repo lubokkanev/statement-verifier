15f56c290731075a8bfa9df1311c569bb07e92f8
niki <>
2016-08-30 15:53:06 +0300

    moved compare for Strings and Numbers in another function

--- src/com/nvl/parser/statement/StatementProcessorImpl.java
+++ src/com/nvl/parser/statement/StatementProcessorImpl.java
@@ -18,8 +18,7 @@ public class StatementProcessorImpl implements StatementProcessor {
 
     @Override
     public boolean verifyStatement(String statement) {
-        StringBuilder valueStatement = new StringBuilder(statement);
-
+        StringBuilder valueStatement = new StringBuilder(statement);        
         for (int i = 0; i < valueStatement.length(); ++i) {
             char character = valueStatement.charAt(i);
 
@@ -32,13 +31,11 @@ public class StatementProcessorImpl implements StatementProcessor {
                 valueStatement.replace(i, i + 1, variableManager.getVariable(variable).getValue());
             }
         }
-
         return correct(valueStatement);
     }
-    
-    
+
     //creates the Reverse Polish Notation
-    private String createRPN(String input) {  
+    private String createRPN(String input) {
         StringBuilder result = new StringBuilder();   //builder for the final result (RPN)
         Stack<Character> operationStack = new Stack<>();  //sttack for the operation
         char[] charInput = input.toCharArray();  //char array for the input
@@ -55,12 +52,12 @@ public class StatementProcessorImpl implements StatementProcessor {
                     while (!operationStack.empty()
                             && (operationStack.peek() == '*' || operationStack.peek() == '/')) {   //if the previous operations in the stack have higher priorities
                         result.append(' ').append(operationStack.pop());                          // add them to result
-                    } 
+                    }
                 case '*':
                 case '/':
-                    result.append(' ');   
+                    result.append(' ');
                 case '(':
-                    operationStack.push(charInput[i]);  
+                    operationStack.push(charInput[i]);
                 case ' ':
                     break;
                 case ')':
@@ -82,10 +79,9 @@ public class StatementProcessorImpl implements StatementProcessor {
         }  //end of while
         return result.toString();  //return resulter RPN
     }  //end of create RPN
-    
-    
+
     //calculates the value of the expresion by RPN
-    private Double calculateRPN(String input) {   
+    private Double calculateRPN(String input) {
         StringTokenizer tokens = new StringTokenizer(input);  //tokenize the input by ' '
         Stack<Double> stack = new Stack<>();  //stack for the numbers
         while (tokens.hasMoreTokens()) {   //while we have more tokens
@@ -113,13 +109,12 @@ public class StatementProcessorImpl implements StatementProcessor {
         }     //end of while
         return stack.pop();   //the result is in the stack(last element)
     }   //end of calculate RPN
-    
-    
+
     //finds the operation of the statement
-    private String parseOperation(String input) {  
+    private String parseOperation(String input) {
         char[] charInput = input.toCharArray();
         int i = 0;
-        while (charInput[i]!='=' && charInput[i]!='>' && charInput[i]!='<' && charInput[i]!='!') {  //iterrate while input[i] is not an operation symbol
+        while (charInput[i] != '=' && charInput[i] != '>' && charInput[i] != '<' && charInput[i] != '!') {  //iterrate while input[i] is not an operation symbol
             i++;
         }
         if (charInput[i + 1] != '=') {    //if operation is > or <
@@ -128,10 +123,9 @@ public class StatementProcessorImpl implements StatementProcessor {
             return input.substring(i, i + 2);
         }//end if
     } //end of parseOperation
-    
-    
+
     /*this works only for numbers*/
-    private boolean correctForNumbers(StringBuilder builder){
+    private boolean correctForNumbers(StringBuilder builder) {
         String input = builder.toString();   //convert builder to string
         String operation = parseOperation(input);   //deternmine the operation (==, >=, <, !=)
         String[] split = input.split(operation);   // split the input by the operation
@@ -141,28 +135,47 @@ public class StatementProcessorImpl implements StatementProcessor {
         String rightRPN = createRPN(rightString);  //create Reverse Polish Notation for the right argument
         Double left = calculateRPN(leftRPN);    //calculate the values
         Double right = calculateRPN(rightRPN);
-        switch (operation) {   //switch for the operation
-            case "==":
-                return left.equals(right);
-            case ">":
-                return left > right;
-            case ">=":
-                return left >= right;
-            case "<":
-                return left < right;
-            case "<=":
-                return left <= right;
-            case "!=":
-                return !left.equals(right);
-            default:
-                return true;
-        }     //end of switch
+        return compare(left, right, operation);
     }   //end of correctForNumbers
 
+    //only for strings
+    private boolean correctForStrings(StringBuilder builder) {
+        String input = builder.toString();
+        String operation = parseOperation(input);   //determine the operation (==, <=, >, !=)
+        String[] split = input.split(operation);   //split by the operation
+        String leftExpreion = split[0].trim();            //left expresion
+        String rightExpresion = split[1].trim();           //right expression
+        //TODO parse expresions
+
+        String left = leftExpreion;
+        String right = rightExpresion;
+        return compare(left, right, operation);
+    }  //end of correctForStrings
+    
+    
+    //execute operation for Strings and Numbers
+    private <E extends Comparable<E>> boolean compare(E left, E right, String operation){
+        switch (operation) {
+            case "==":
+                return left.compareTo(right) == 0;
+            case ">":
+                return left.compareTo(right) > 0;
+            case "<":
+                return left.compareTo(right) < 0;
+            case ">=":
+                return left.compareTo(right) >= 0;
+            case "<=":
+                return left.compareTo(right) >= 0;
+            case "!=":
+                return left.compareTo(right) != 0;
+            default:
+                return false;
+        }   //end of switch
+    }   //end of compare
+    
     
     private boolean correct(StringBuilder builder) {
         // TODO: Niki / Lubo
-        
-        return correctForNumbers(builder);
+        return correctForStrings(builder);
     }
 }
